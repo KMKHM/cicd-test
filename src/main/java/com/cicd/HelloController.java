02829110c631803c1684d3ac742ea0b39dee50d2
KMK_HM <kyun9151@gmail.com>
2025-07-14 21:10:54 +0900

    test

--- src/main/java/com/cicd/HelloController.java
+++ src/main/java/com/cicd/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String hello() {
-        return "Hello, World! JENKINS END!!";
+        return "Hello, World! JENKINS END, Please!!";
     }
 
     @GetMapping("/bye")
